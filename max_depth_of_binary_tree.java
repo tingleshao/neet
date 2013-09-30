d5b8eb852454335b6daf1f578cb9ccc57c7dd03e
my ubuntu <tingleshao@gmail.com>
2013-09-30 11:19:06 -0400

    first blood

---
+++ max_depth_of_binary_tree.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ /*
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if (root == null) {
+            return 0; 
+        }
+        else {
+            if( root.left == null && root.right == null ) {
+               return 1; }
+            else {
+               return 1 + Math.max(maxDepth(root.left), maxDepth(root.right)); 
+            }
+        }
+    }
+}
