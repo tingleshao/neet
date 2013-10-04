d2031ff781d5e41ef5ba2db80ae3e6b5aaa343e6
my ubuntu <tingleshao@gmail.com>
2013-10-03 21:54:36 -0400

    best time to buy and sell stock

---
+++ best_time_to_buy_and_sell_stock_II.java
@@ -0,0 +1,25 @@
+public class Solution {
+
+   public int maxProfit(int[] prices) {
+
+       // Note: The Solution object is instantiated only once and is reused by each test case.
+
+       int profit = 0;
+
+       for (int i = 0; i < prices.length-1; i++) {
+
+           if (prices[i+1] > prices[i] ) {
+
+               profit += prices[i+1] - prices[i];
+
+           }
+
+       }
+
+       return profit;
+
+   }
+
+}
+
+
